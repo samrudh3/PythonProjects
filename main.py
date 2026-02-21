fa36e70947b8485555c0407df75b1f6983535b5e
samrudh3 <samrudhgr3@gmail.com>
2026-02-21 19:05:29 +0530

    Loops, break, continue

--- main.py
+++ main.py
@@ -44,12 +44,49 @@
 # else :
 #     print("Good Night")      
 
-x = 5
+# x = 5
 
-match x:
-    case 0:
-        print("x is",x)
-    case 1:
-        print("x is", x)
-    case _ if(x == 5):
-        print("Ohh it is 5 ") 
+# match x:
+#     case 0:
+#         print("x is",x)
+#     case 1:
+#         print("x is", x)
+#     case _ if(x == 5):
+#         print("Ohh it is 5 ") 
+
+# for i in range(5): #Prints 0 to 5
+#     print(i)
+
+
+# for i in range(1,5): #Starts from 1
+#     print(i)
+
+# for i in range(1, 11, 2): #Step index 2
+#     print(i)
+
+# i = 0 
+# while(i < 5):
+#     print(i)
+#     i = i + 1
+
+# i = 5
+# while i > 0 :
+#     print(i)
+#     i -= 1
+# else:
+#     print("while loop else")    
+
+# for i  in range(5):
+#     if i == 2:
+#         continue
+#     print(i)
+# else:
+#     print("for loop else")
+
+#Do while in python
+# i = 0
+# while True:
+#     print(i)
+#     i = i + 1
+#     if i == 1:
+#         break

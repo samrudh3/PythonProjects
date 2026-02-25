fff4a3d05a65111cc5dee2d3aeb85d36e3ad52fe
samrudh3 <samrudhgr3@gmail.com>
2026-02-25 23:18:23 +0530

    Constructor and decorator

--- OOPS/oops.py
+++ OOPS/oops.py
@@ -1,14 +1,35 @@
-class Person:
-    name = "Harry"
-    occupation = "Engineer"
+# class Person:
+#     def __init__(self,name, occ):
+#         self.name = name
+#         self.occupation = occ
 
-    def info(self):
-        print(f"{self.name} is a {self.occupation}")
+#     def info(self):
+#         print(f"{self.name} is a {self.occupation}")
 
-a = Person()
-a.name = "Abhi"
-a.occupation = "Doctor"
-a.info()
+# a = Person()
+# a.name = "Abhi"
+# a.occupation = "Doctor"
+# a.info()
 
-b = Person()
-b.info()
+# b = Person()
+# b.info()
+
+# a = Person("Harry", "Enginner")
+# a.info()
+
+# b = Person("Diksha", "HR")
+# b.info()
+
+def greet(fx):
+    def mfx(*args):
+        print("Hello")
+        fx(*args)
+        print("Bye")
+    return mfx
+
+@greet
+def add(a,b):
+    print(a+b)
+
+add(2,3)
+# greet(add)(1,2) #This also gives same output, need to remove @greet 

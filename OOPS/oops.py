a89543ab5d397529e8d11401ab68a5b95926de1c
samrudh3 <samrudhgr3@gmail.com>
2026-03-02 00:47:06 +0530

    method overriding

--- OOPS/oops.py
+++ OOPS/oops.py
@@ -247,3 +247,24 @@
 # print(str(ex))
 # print(repr(ex))
 # ex()         
+
+# class shape:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+
+#     def area(self):    
+#         return self.x * self.y
+    
+# class circle(shape):
+#     def __init__(self, x, y):
+#         super().__init__(x, y)
+
+#     def area(self):
+#         return 3.14 * super().area()        
+
+# rec = shape(3,4)
+# print(rec.area())
+
+# c = circle(3,4)
+# print(c.area())

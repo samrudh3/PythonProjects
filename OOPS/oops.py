4c8ede0942ae86e3d8c98e836b50ecac9ecaf12a
samrudh3 <samrudhgr3@gmail.com>
2026-03-02 20:22:12 +0530

    Merge pdf Exerisce

--- OOPS/oops.py
+++ OOPS/oops.py
@@ -315,3 +315,16 @@
 
 # print(Example.mro())
         
+
+#Merge pdf Exercise 
+# from PyPDF2 import PdfWriter
+# import os
+
+# merger = PdfWriter()
+# files = [files for file in os.listdir() if file.endswith(".pdf")]
+
+# for pdf in files:
+#     merger.append(pdf)
+
+# merger.write("Merged.pdf")
+# merger.close()    

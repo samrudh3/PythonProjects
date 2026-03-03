828ea77be2345c7bff2d95bf932489a4b8a33ca5
samrudh3 <samrudhgr3@gmail.com>
2026-03-03 23:09:46 +0530

    requets and bs4 module

--- Mid/medium.py
+++ Mid/medium.py
@@ -47,3 +47,31 @@
 # shutil.copytree("src", "dst") #recursilvely copeis the directory from src to dst
 # shutil.move("src", "dst") # Moves the file from src to dst (can we used to rename also)
 # shutil.rmtree("path")# Helps to remove the dir(can't delete file)
+
+# import requests
+# from bs4 import BeautifulSoup
+
+# url = "https://www.codewithharry.com/blogpost/django-cheatsheet/"
+
+# r = requests.get(url)
+
+# soup = BeautifulSoup(r.text, 'html.parser')
+# # print(soup.prettify())
+
+# for heading in soup.find_all("h2"):
+#     print(heading.text)
+# print(r.text)
+
+# url = "https://jsonplaceholder.typicode.com/posts"
+
+# data = {
+#     "title": 'harry',
+#     "body": 'bhai',
+#     "userId": 12,
+#   }
+# headers =  {
+#     'Content-type': 'application/json; charset=UTF-8',
+#   }
+# response = requests.post(url, headers=headers, json=data)
+
+# print(response.text)
